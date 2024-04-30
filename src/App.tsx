feb655b119a968ada41286a0d5c5dc73442526db
Lymore01 <kellytoroitich89@gmail.com>
2024-04-30 14:15:05 +0300

    framer motion

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,25 @@
-import './App.css'
-import TestStore from './Components/TestStore'
-import ViewStore from './Components/ViewStore'
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import "./App.css";
+import AnimateControls from "./Components/AnimateControls";
+import FramerMotion from "./Components/FramerMotion";
+import Gestures from "./Components/Gestures";
+import ScrollBasedAnimation from "./Components/ScrollBasedAnimation";
+import ViewBasedMotion from "./Components/ViewBasedMotion";
+/* import TestStore from './Components/TestStore'
+import ViewStore from './Components/ViewStore' */
 
 function App() {
-  
-
   return (
     <>
-      <TestStore />
-      <ViewStore />
+      {/* <TestStore />
+      <ViewStore /> */}
+      {/* <FramerMotion /> */}
+      {/* <Gestures /> */}
+      {/* <AnimateControls /> */}
+      {/* <ViewBasedMotion /> */}
+      <ScrollBasedAnimation />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
